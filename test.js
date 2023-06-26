2532a999564a19a8e172df7dc24ca7f51815ac86
Ashay Changwani <ashaychangwani@gmail.com>
2023-06-26 03:01:21 -0400

    Added mixed chart

--- test.js
+++ test.js
@@ -4,10 +4,7 @@ const { openaiApiCall } = require('./openai');
 
 (async () => {
     try {
-        const data = 'Here is the Mauritius population pyramid 2011:\
-        Age Ranges: 85+, 80-84, 75-79, 70-74, 65-69, 60-64, 55-59, 50-54, 45-49, 40-44, 35-39, 30-34, 25-29, 20-24, 15-19, 10-14, 5-9, 0-4\
-        Males: 0.4, 0.65, 0.76, 0.88, 1.5, 2.1, 2.9, 3.8, 3.9, 4.2, 4, 4.3, 4.1, 4.2, 4.5, 3.9, 3.5, 3\
-        Females: -0.8, -1.05, -1.06, -1.18, -1.4, -2.2, -2.85, -3.7, -3.96, -4.22, -4.3, -4.4,-4.1, -4, -4.1, -3.4, -3.1, -2.8'
+        const data = "For Team A, the chart shows a column chart with the corresponding values: 23, 11, 22, 27, 13, 22, 37, 21, 44, 22, and 30. Team B's data is represented as an area chart, and the values are 44, 55, 41, 67, 22, 43, 21, 41, 56, 27, and 43. Lastly, Team C's data is presented as a line chart, with the values 30, 25, 36, 30, 45, 35, 64, 52, 59, 36, and 39."
 
         const response = await openaiApiCall(data);
         console.log(response.data.choices[0].message.function_call.arguments)
