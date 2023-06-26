8ee094b91f7bfaa8fdb857b1d73497870265e00c
Ashay Changwani <ashaychangwani@gmail.com>
2023-06-26 02:07:04 -0400

    Add support for line and area charts

--- openai.js
+++ openai.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const axios = require('axios');
-
-const openaiApiCall = async (data, functions) => {
+const fs = require('fs');
+function loadJSONFromFile(filename) {
+    const rawData = fs.readFileSync(filename);
+    const jsonData = JSON.parse(rawData);
+    return jsonData;
+  }
+const openaiApiCall = async (data) => {
   const apiKey = process.env.OPENAI_API_KEY;
-  const prompt = `Plot a bar chart from this data: ${data}`;
-
+  const prompt = `Plot a line chart from this data: ${data}`;
+  const functions = [ loadJSONFromFile('barchart.json') ]
   try {
     const response = await axios.post(
       'https://api.openai.com/v1/chat/completions',
