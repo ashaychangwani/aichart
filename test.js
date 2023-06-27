cc1d2abb08e4bca2a795356cd8b5c8f79a7adbcb
Ashay Changwani <ashaychangwani@gmail.com>
2023-06-27 00:59:29 -0400

    impl donut chart

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { openaiApiCall } = require('./openai');
 
 (async () => {
     try {
-        const data = 'Category A constitutes 35% of the total, Category B accounts for 20%, Category C represents 15%, Category D represents 10%, and Category E accounts for the remaining 20%.'
+        const data = 'Make a gradient chart with this - Category A constitutes 35% of the total, Category B accounts for 20%, Category C represents 15%, Category D represents 10%, and Category E accounts for the remaining 20%.'
 
         const response = await openaiApiCall(data);
         console.log(response.data.choices[0].message.function_call.arguments)
